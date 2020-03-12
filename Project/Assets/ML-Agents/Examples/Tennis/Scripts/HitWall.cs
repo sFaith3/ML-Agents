51e24769007958141fb5135b110d5f19b2aefa21
David <davidrecuencooliver@enti.cat>
2020-03-12 20:01:34 +0100

    Paddle try

--- Project/Assets/ML-Agents/Examples/Tennis/Scripts/HitWall.cs
+++ Project/Assets/ML-Agents/Examples/Tennis/Scripts/HitWall.cs
@@ -14,6 +14,10 @@ public class HitWall : MonoBehaviour
     private bool hasTouchedAgent;
     private bool firstTouch;
 
+    private enum Status {
+        Floor, Agent, Wall
+    };
+    Status state;
 
     TennisArea m_Area;
     TennisAgent m_Agent;
@@ -23,12 +27,13 @@ public class HitWall : MonoBehaviour
     {
         m_Area = areaObject.GetComponent<TennisArea>();
         m_Agent = m_Area.agent.GetComponent<TennisAgent>();
-        hasTouchedFloor = false;
+        hasTouchedFloor = true;
         hasTouchedWall = false;
         hasTouchedAgent = false;
         firstTouch = true;
         currentReward = 0;
         currentLoses = 0;
+        state = Status.Floor;
     }
 
     void Reset()
@@ -36,17 +41,18 @@ public class HitWall : MonoBehaviour
         m_Agent.Done();
         m_Area.MatchReset();
         net = false;
-        hasTouchedFloor = false;
+        hasTouchedFloor = true; // Evita que haga dos botes al principio
         hasTouchedWall = false;
         firstTouch = true;
         currentReward = 0;
         currentLoses++;
+        state = Status.Floor;
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name == "Floor") {
-            if (!hasTouchedFloor) {
+        /*if (collision.gameObject.name == "Floor") {
+            if (hasTouchedWall) {
                 hasTouchedFloor = true;
             } else if (hasTouchedFloor) { // segundo bote
                 if (firstTouch) {
@@ -59,37 +65,88 @@ public class HitWall : MonoBehaviour
             }
             
         } else if(collision.gameObject.name == "WallFront") {
-            if(hasTouchedAgent) hasTouchedWall = true;
+            if (hasTouchedAgent) {
+                hasTouchedWall = true;
+            }
         } else if (collision.gameObject.name == "Agent") { // Toca raqueta
             if (firstTouch) {
                 //currentReward += 2; // Boost al principio
                 m_Agent.AddReward(2);
                 firstTouch = false;
+                hasTouchedAgent = true;
             } else if (hasTouchedWall && hasTouchedFloor) {
                 hasTouchedWall = false;
-                hasTouchedFloor = false;
                 //currentReward++;
                 m_Agent.AddReward(1);
+                hasTouchedAgent = true;
             }
-            hasTouchedAgent = true;
-        }
-        /*else if (collision.gameObject.name == "AgentB")
-        {
-            // Agent B double hit
-            if (lastAgentHit == 1)
-            {
-                AgentAWins();
-            }
-            else
-            {
-                if (lastFloorHit != FloorHit.Service && !net)
-                {
-                    net = true;
-                }
-
-                lastAgentHit = 1;
-                lastFloorHit = FloorHit.FloorHitUnset;
-            }
+            
         }*/
+
+        if (collision.gameObject.name == "Floor") {
+            switch (state) {
+                case Status.Floor: // Doble bote en el suelo
+                    Death(-1);
+                    break;
+
+                case Status.Agent: // De raqueta a suelo
+                    Death(-1);
+                    break;
+
+                case Status.Wall:
+                    state = Status.Floor;
+                    break;
+            }
+        }
+        else if (collision.gameObject.name == "WallFront") {
+            switch (state) {
+                case Status.Floor:
+                    Death(-1);
+                    break;
+
+                case Status.Agent:
+                    state = Status.Agent;
+                    break;
+
+                case Status.Wall: // De pared a pared
+                    Death(-5);
+                    break;
+            }
+        }
+        else if (collision.gameObject.name == "Agent") {
+            switch (state) {
+                case Status.Floor: // Le da tras un bote
+                    if (firstTouch) {
+                        m_Agent.AddReward(2);
+                        firstTouch = false;
+                    } else if (!firstTouch) {
+                        m_Agent.AddReward(1);
+                    }
+                    state = Status.Agent;
+                    break;
+
+                case Status.Agent: // Doble hit
+                    Death(-0.5f);
+                    break;
+
+                case Status.Wall: // Hit directo, ha de esperar a que toque el suelo
+                    Death(-0.5f);
+                    break;
+            }
+
+        }
+
+        
+
+    }
+
+    public void Death(float extra) {
+        if (firstTouch) {
+            if (currentLoses >= 2500) m_Agent.AddReward(-4); //currentReward -= 3;
+            else if (currentLoses < 2500) m_Agent.AddReward(-1); //currentReward--; // Extra if no touch
+        }
+        //m_Agent.AddReward(currentReward - 1);
+        m_Agent.AddReward(extra);
+        Reset();
     }
 }
