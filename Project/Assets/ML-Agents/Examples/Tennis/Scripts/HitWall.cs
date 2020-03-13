f149e2cce1802dbc4e52858ceaeee4acb7ff9967
David Recuenco <thedavidro@gmail.com>
2020-03-13 12:15:43 +0100

    Paddle Done

--- Project/Assets/ML-Agents/Examples/Tennis/Scripts/HitWall.cs
+++ Project/Assets/ML-Agents/Examples/Tennis/Scripts/HitWall.cs
@@ -13,9 +13,13 @@ public class HitWall : MonoBehaviour
     private bool hasTouchedWall;
     private bool hasTouchedAgent;
     private bool firstTouch;
+    
+    private float checkLostGames = 1500f;
+    private float count = 0;
+    private float goodReward = 5;
 
     private enum Status {
-        Floor, Agent, Wall
+        Service, Floor, Agent, Wall
     };
     Status state;
 
@@ -27,13 +31,14 @@ public class HitWall : MonoBehaviour
     {
         m_Area = areaObject.GetComponent<TennisArea>();
         m_Agent = m_Area.agent.GetComponent<TennisAgent>();
-        hasTouchedFloor = true;
-        hasTouchedWall = false;
-        hasTouchedAgent = false;
-        firstTouch = true;
+        //hasTouchedFloor = true;
+        //hasTouchedWall = false;
+        //hasTouchedAgent = false;
+        //firstTouch = true;
         currentReward = 0;
         currentLoses = 0;
-        state = Status.Floor;
+        state = Status.Service;
+        count = 0;
     }
 
     void Reset()
@@ -41,12 +46,12 @@ public class HitWall : MonoBehaviour
         m_Agent.Done();
         m_Area.MatchReset();
         net = false;
-        hasTouchedFloor = true; // Evita que haga dos botes al principio
-        hasTouchedWall = false;
-        firstTouch = true;
+        //hasTouchedFloor = false; // Evita que haga dos botes al principio //estaba en true
+        //hasTouchedWall = false;
+        //firstTouch = true;
         currentReward = 0;
         currentLoses++;
-        state = Status.Floor;
+        state = Status.Service;
     }
 
     void OnCollisionEnter(Collision collision)
@@ -83,8 +88,16 @@ public class HitWall : MonoBehaviour
             
         }*/
 
-        if (collision.gameObject.name == "Floor") {
-            switch (state) {
+        if (collision.gameObject.name == "Floor") { 
+            switch (state)
+            {
+                case Status.Service: //saque
+                    //if (CheckLostTooMuchGames(checkLostGames)) Death(-4); //poner negativo mas potente?
+                    //else Death(-1);
+                    count++;
+                    Death(-count * 4);
+                    break;
+
                 case Status.Floor: // Doble bote en el suelo
                     Death(-1);
                     break;
@@ -94,43 +107,62 @@ public class HitWall : MonoBehaviour
                     break;
 
                 case Status.Wall:
+                    //WellDone(0.5f);//No hace falta pq no es accion d la IA
                     state = Status.Floor;
                     break;
             }
         }
         else if (collision.gameObject.name == "WallFront") {
             switch (state) {
+                case Status.Service: //no deberia ocurrir pero por si aka
+                    //if (CheckLostTooMuchGames(checkLostGames)) Death(-4); 
+                    //else Death(-1);
+                    Death(-3);
+                    break;
+
                 case Status.Floor:
                     Death(-1);
                     break;
 
                 case Status.Agent:
-                    state = Status.Agent;
+                    WellDone(2);
+                    state = Status.Wall; //Tenias --> state.Agent //dar refuerzo positivo
                     break;
 
                 case Status.Wall: // De pared a pared
-                    Death(-5);
+                    Death(-3);
                     break;
             }
         }
         else if (collision.gameObject.name == "Agent") {
             switch (state) {
+                case Status.Service:
+                    float reward = goodReward;
+                    reward -= count;
+                    if (reward <= 0) reward = 1;
+
+                    WellDone(goodReward);
+                    count = 0;
+                    state = Status.Agent;
+                    break;
+
                 case Status.Floor: // Le da tras un bote
-                    if (firstTouch) {
-                        m_Agent.AddReward(2);
-                        firstTouch = false;
-                    } else if (!firstTouch) {
-                        m_Agent.AddReward(1);
-                    }
+                    /* if (firstTouch) {
+                         m_Agent.AddReward(2); //esto pq amor?! sigues haciendo q bote en el saque la pelota!!!
+                         firstTouch = false;
+                     } else if (!firstTouch) {
+                         m_Agent.AddReward(1);
+                     }*/
+                    WellDone(2);
                     state = Status.Agent;
                     break;
 
                 case Status.Agent: // Doble hit
-                    Death(-0.5f);
+                    Death(-1f);
                     break;
 
                 case Status.Wall: // Hit directo, ha de esperar a que toque el suelo
-                    Death(-0.5f);
+                    Death(-1f);
                     break;
             }
 
@@ -140,13 +172,22 @@ public class HitWall : MonoBehaviour
 
     }
 
-    public void Death(float extra) {
-        if (firstTouch) {
-            if (currentLoses >= 2500) m_Agent.AddReward(-4); //currentReward -= 3;
-            else if (currentLoses < 2500) m_Agent.AddReward(-1); //currentReward--; // Extra if no touch
-        }
+    public void Death(float negativeReward) {
+        /*if (state == Status.Service) {
+            if (currentLoses >= checkLoses) m_Agent.AddReward(-4); //currentReward -= 3;
+            else if (currentLoses < checkLoses) m_Agent.AddReward(-1); //currentReward--; // Extra if no touch
+        }*/
         //m_Agent.AddReward(currentReward - 1);
-        m_Agent.AddReward(extra);
+        m_Agent.AddReward(negativeReward);
         Reset();
+    }
+    public void WellDone(float positiveReward)
+    {
+        m_Agent.AddReward(positiveReward);
+    }
+    public bool CheckLostTooMuchGames(float lostGames)
+    {
+        if (currentLoses >= lostGames) return true;
+        else return false;
     }
 }
