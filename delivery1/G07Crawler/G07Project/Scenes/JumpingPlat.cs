eb6c62b8c9226eb57730657fb42abe95e6cfa9aa
Nil <nilbellmuntperez@enti.cat>
2020-05-28 16:45:19 +0200

    moved

---
+++ delivery1/G07Crawler/G07Project/Scenes/JumpingPlat.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingPlat : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 40000, 0));
+            Debug.Log("Voladore");
+        }
+    }
+}
